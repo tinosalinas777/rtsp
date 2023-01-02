dffc80ee924bc772080f9c16b64e1c2478ea9a11
Cristian Salinas <62073149+tinosalinas777@users.noreply.github.com>
2023-01-02 19:18:14 -0300

    Add files via upload

--- ffmpegPython.py
+++ ffmpegPython.py
@@ -4,42 +4,49 @@ import threading
 import cv2
 import time
 
-promp="\033[3;33m"+"cov>>>"+"\033[0;m"
+promp="\033[1;32m"+"cov>>>"+"\033[0;m"
 banner=pyfiglet.figlet_format("   Cliente RTSP ")#titulo del programa
 
 
 def cam1(x):
     
 
-    os.system("ffplay -hide_banner -loglevel error -i "+x)
+    os.system("ffplay -hide_banner -loglevel error -i "+x)#para obs usar -vf scale=1280:720 escala exacta para captura obs
     
-def cam2(x):
+
     
-    os.system("ffplay -hide_banner -loglevel error -i "+x)
-    #"ffplay -hide_banner -loglevel error -max_delay 500000 "+x
 
 def camaras():
     lista1=open("comandos.txt",mode="r")
+    lista2=open("nombres.txt",mode="r")
+    listaNombres=list()
+    for i in lista2:
+        listaNombres.append(i)
     listaComandos=list()
     for i in lista1:
         listaComandos.append(i)
     lista1.close()
-    print("\n\n    ********* Desarrollado por oficina tecnica COV ******")
+    print("\033[1;32m"+"\n\n    ********* Desarrollado por oficina tecnica COV ******""\033[0;m")
     print(banner)
-    print("   selecione camara\n 0- canal 26 \n 1- pasillo \n 2- racks\n 3- cerrar previo\n ")
+    print("\033[1;32m"+"   selecione camara\n "+"\033[0;m")
+
+    for i in listaNombres:
+        print(i)
     try:
         x=int(input(promp))
+        if x ==99:
+            os.system("cls")
+            return
 
-        print("selecione previo\n\n ")
-        z=int(input(promp))
+       
         os.system("cls")
         l=listaComandos[x]
-        m=listaComandos[z]
+    
 
         t1=threading.Thread(target=cam1, args=(l,))
-        t2=threading.Thread(target=cam2, args=(m,))
+        
         t1.start()
-        t2.start()
+        
         
         
     except ValueError:
@@ -78,9 +85,9 @@ while menu !=0:
             grabaEscritorio()
         elif menu ==4:
             exit()
-        print("\n\n    ********* Desarrollado por oficina tecnica COV ******")
+        print("\033[1;32m"+"\n\n    ********* Desarrollado por oficina tecnica COV ******""\033[0;m")
         print (banner)
-        menu=int(input("             ******** Cliente RTSP ********\n \n 1- Seleccionar camaras \n 2- recepcion canal 26 por rtsp \n 3- grabar escritorio\n 4- Salir\n\n" +promp))
+        menu=int(input("\033[1;32m"+"             ******** Cliente RTSP ********\n \n "+"\033[0;m"+"1- Seleccionar camaras \n 2- recepcion canal 26 por rtsp \n 3- grabar escritorio\n 4- Salir\n\n"+promp))
         os.system("cls")
     except ValueError:
         print("\033[4;31m"+"la entrada debe ser un numero"+"\033[0;m")
